28def5e86d4e55362c2e721133036d6654aea6f6
Mark Morga <markmorga@gmail.com>
2025-03-20 21:44:43 -0500

    deliverable and gap

--- src/components/main/view-nodes/deliverable-view-node.tsx
+++ src/components/main/view-nodes/deliverable-view-node.tsx
@@ -1,48 +1,63 @@
+import { ViewNode } from "../../../archimate-model";
 import { JSX } from "react";
-import DefaultViewNode, { IViewNodeProps } from "./default-element";
+import * as BaseViewNode from "./base-view-node";
+import React, { useEffect, useState } from "react";
 
-export default class DeliverableViewNode extends DefaultViewNode {
-  constructor(props: IViewNodeProps) {
-    super(props);
-    this.state = {
-      ...this.state,
-      badge: "archimate-artifact-badge"
-    };
-  }
+export const DeliverableViewNode: React.FC<BaseViewNode.IViewNodeProps> = React.memo((props) => {
 
-  public entityShape(): JSX.Element {
-    const bounds = this.props.viewNode.absolutePosition();
-    return (
-      <>
-        <path
-          d={[
-            "M",
-            bounds.left,
-            bounds.top,
-            "v",
-            bounds.height - 8,
-            "c",
-            0.167 * bounds.width,
-            0.133 * bounds.height,
-            0.336 * bounds.width,
-            0.133 * bounds.height,
-            bounds.width * 0.508,
-            0,
-            "c",
-            0.0161 * bounds.width,
-            -0.0778 * bounds.height,
-            0.322 * bounds.width,
-            -0.0778 * bounds.height,
-            bounds.width * 0.475,
-            0,
-            "v",
-            -(bounds.height - 8),
-            "z"
-          ].join(" ")}
-          className={this.state.backgroundClass}
-          style={this.shapeStyle()}
-        />
-      </>
-    );
-  }
+  const [state, setState] = useState<BaseViewNode.IViewNodeState>(
+    BaseViewNode.initialState(props.viewNode, {
+      badge: "archimate-artifact-badge",
+      textBounds: BaseViewNode.textBounds(props.viewNode, props.x, props.y),
+      entityShape: entityShape
+    }));
+
+  useEffect(() => {
+    if (props.x !== undefined || props.y !== undefined) {
+      setState(prevState => ({
+        ...prevState,
+        textBounds: BaseViewNode.textBounds(props.viewNode, props.x, props.y)
+      }));
+    }
+  }, [props.x, props.y, props.viewNode]);
+
+  return BaseViewNode.render(props, state);
+});
+
+export function entityShape(viewNode: ViewNode, backgroundClass: string | undefined, shapeStyle: React.CSSProperties | undefined): JSX.Element {
+  const bounds = viewNode.absolutePosition();
+  return (
+    <>
+      <path
+        d={[
+          "M",
+          bounds.left,
+          bounds.top,
+          "v",
+          bounds.height - 8,
+          "c",
+          0.167 * bounds.width,
+          0.133 * bounds.height,
+          0.336 * bounds.width,
+          0.133 * bounds.height,
+          bounds.width * 0.508,
+          0,
+          "c",
+          0.0161 * bounds.width,
+          -0.0778 * bounds.height,
+          0.322 * bounds.width,
+          -0.0778 * bounds.height,
+          bounds.width * 0.475,
+          0,
+          "v",
+          -(bounds.height - 8),
+          "z"
+        ].join(" ")}
+        className={backgroundClass}
+        style={shapeStyle}
+      />
+    </>
+  );
 }
+
+export default DeliverableViewNode;
--- src/components/main/view-nodes/gap-view-node.tsx
+++ src/components/main/view-nodes/gap-view-node.tsx
@@ -1,24 +1,40 @@
 import { Bounds } from "../../../archimate-model";
-import { IViewNodeProps } from "./default-element";
-import DeliverableViewNode from "./deliverable-view-node";
+import * as DeliverableViewNode from "./deliverable-view-node";
+import { ViewNode } from "../../../archimate-model";
+import * as BaseViewNode from "./base-view-node";
+import React, { useEffect, useState } from "react";
 
-export default class GapViewNode extends DeliverableViewNode {
-  constructor(props: IViewNodeProps) {
-    super(props);
-    this.state = {
-      ...this.state,
+export const GapViewNode: React.FC<BaseViewNode.IViewNodeProps> = React.memo((props) => {
+
+  const [state, setState] = useState<BaseViewNode.IViewNodeState>(
+    BaseViewNode.initialState(props.viewNode, {
       backgroundClass: "archimate-implementation2-background",
       badge: "#archimate-gap-badge",
-      badgeBounds: this.badgeBounds()
-    };
-  }
+      badgeBounds: badgeBounds(props.viewNode),
+      textBounds: BaseViewNode.textBounds(props.viewNode, props.x, props.y),
+      entityShape: DeliverableViewNode.entityShape
+    }));
 
-  protected badgeBounds(): Bounds | undefined {
-    return new Bounds(
-      this.props.viewNode.absolutePosition().right - 25,
-      this.props.viewNode.absolutePosition().top + 5,
-      20,
-      20
-    );
-  }
+  useEffect(() => {
+    if (props.x !== undefined || props.y !== undefined) {
+      setState(prevState => ({
+        ...prevState,
+        badgeBounds: badgeBounds(props.viewNode),
+        textBounds: BaseViewNode.textBounds(props.viewNode, props.x, props.y)
+      }));
+    }
+  }, [props.x, props.y, props.viewNode]);
+
+  return BaseViewNode.render(props, state);
+});
+
+function badgeBounds(viewNode: ViewNode): Bounds | undefined {
+  return new Bounds(
+    viewNode.absolutePosition().right - 25,
+    viewNode.absolutePosition().top + 5,
+    20,
+    20
+  );
 }
+
+export default GapViewNode;
