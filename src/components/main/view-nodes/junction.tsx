ba254ae1da67755fc85fe31d2e123768f64ca675
Mark Morga <markmorga@gmail.com>
2025-03-20 22:03:10 -0500

    junction and or junction

--- src/components/main/view-nodes/junction.tsx
+++ src/components/main/view-nodes/junction.tsx
@@ -1,30 +1,47 @@
-import DefaultViewNode, { IViewNodeProps } from "./default-element";
+import { ViewNode } from "@/archimate-model";
+import { JSX } from "react";
+import * as BaseViewNode from "./base-view-node";
+import React, { useEffect, useState } from "react";
 
-export default class JunctionViewNode extends DefaultViewNode {
-  constructor(props: IViewNodeProps) {
-    super(props);
-    this.state = {
-      ...this.state,
-      backgroundClass: "archimate-junction-background"
-    };
-  }
+export const JunctionViewNode: React.FC<BaseViewNode.IViewNodeProps> = React.memo((props) => {
 
-  protected entityLabel() {
-    return undefined;
-  }
+  const [state, setState] = useState<BaseViewNode.IViewNodeState>(
+    BaseViewNode.initialState(props.viewNode, {
+      backgroundClass: "archimate-junction-background",
+      textBounds: BaseViewNode.textBounds(props.viewNode),
+      entityShape: entityShape,
+      entityLabel: entityLabel
+    }));
 
-  protected entityShape() {
-    const bounds = this.props.viewNode.absolutePosition();
-    const center = bounds.center();
-    const r = Math.min(bounds.width, bounds.height) / 2;
-    return (
-      <circle
-        cx={center.x}
-        cy={center.y}
-        r={r}
-        className={this.state.backgroundClass}
-        style={this.shapeStyle()}
-      />
-    );
-  }
+  useEffect(() => {
+    if (props.x !== undefined || props.y !== undefined) {
+      setState(prevState => ({
+        ...prevState,
+        textBounds: BaseViewNode.textBounds(props.viewNode)
+      }));
+    }
+  }, [props.x, props.y, props.viewNode]);
+
+  return BaseViewNode.render(props, state);
+});
+
+export function entityLabel() {
+  return undefined;
 }
+
+export function entityShape(viewNode: ViewNode, backgroundClass: string | undefined, shapeStyle: React.CSSProperties | undefined): JSX.Element {
+  const bounds = viewNode.absolutePosition();
+  const center = bounds.center();
+  const r = Math.min(bounds.width, bounds.height) / 2;
+  return (
+    <circle
+      cx={center.x}
+      cy={center.y}
+      r={r}
+      className={backgroundClass}
+      style={shapeStyle}
+    />
+  );
+}
+
+export default JunctionViewNode;
