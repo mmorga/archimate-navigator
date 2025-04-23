9d2dd52f8540cb9ca86f5a2762158d2afc043b84
Mark Morga <markmorga@gmail.com>
2025-04-23 14:58:00 -0500

    Updated reference to archimate model to the vite way

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import ArchimateNavigator from "./components/archimate-navigator";
 import "./index.css";
 import { StrictMode } from "react";
+import modelUrl from "/archimate/ArchiSurance V3.archimate?url";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
@@ -11,7 +12,7 @@ const root = createRoot(container);
 
 root.render(
   <StrictMode>
-    <ArchimateNavigator modelUrl="archimate-navigator/archimate/ArchiSurance V3.archimate" />
+    <ArchimateNavigator modelUrl={modelUrl} />
   </StrictMode>,
   // <ArchimateNavigator modelUrl="/archimate/everything.archimate" />,
   // <ArchimateNavigator modelUrl="/archimate/sample.archimate" />
